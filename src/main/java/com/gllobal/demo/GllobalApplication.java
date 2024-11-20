a54b414feba1196234706ceb9f5cc092845bfab2
JavierAlvarezz <121823607+JavierAlvarezz@users.noreply.github.com>
2024-11-19 21:38:56 -0500

    Iniciando Proyecto T1

---
+++ src/main/java/com/gllobal/demo/GllobalApplication.java
@@ -0,0 +1,13 @@
+package com.gllobal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GllobalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GllobalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gllobal/demo/GllobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gllobal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GllobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
